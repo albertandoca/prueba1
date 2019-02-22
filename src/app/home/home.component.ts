2d615ab2fbe9731cd750b11e9b99baa35d86c20c
albertandoca <albertandoca@gmail.com>
2019-02-22 11:09:56 -0500

    estructura

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  mensajeSaludo: String;
+  mostrar: boolean;
+  nombre: String;
+  verificar: boolean;
+  valorBoton: String;
+
   constructor() { }
 
   ngOnInit() {
+    this.mostrar = true;
+    this.nombre = 'Carlos';
+    this.verificar = true;
+    this.valorBoton = 'ocultar bloque';
+
+    setInterval(() => {
+      this.verifica();
+    }, 5000);
+    
+  }
+
+  saludo(saludo: String) {
+    alert(saludo);
+    this.bye();
+  }
+
+  bye() {
+    console.log('Te fuiste muy pronto');
+  }
+
+  contar() {
+    let numeros = [];
+    for (let i = 0; i < 10000; i++){
+      numeros.push(i);
+    };
+    
+    console.log(numeros);
+    this.saludo('hola')
+
+  }
+
+  ver() {
+    this.mostrar = this.mostrar == true ? false : true;
+    
+  }
+
+  verifica() {
+    this.verificar = this.verificar ? false : true;
+    this.valorBoton = this.verificar ? 'ocultar bloque' : 'ver bloque';
+    console.log(this.valorBoton);
   }
 
 }
